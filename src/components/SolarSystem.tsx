603410e7455810a82337b967085b3946e08c3ba8
TatianeMarinho <devtatianemarinho@gmail.com>
2023-06-04 19:39:04 -0400

    a sexta

--- src/components/SolarSystem.tsx
+++ src/components/SolarSystem.tsx
@@ -1,9 +1,17 @@
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import Planets from '../data/planets';
 
 function SolarSystem() {
   return (
     <div data-testid="solar-system">
       <Title headline="Planetas" />
+      {Planets.map((planet) => (
+        <PlanetCard
+          key={ planet.name }
+          planetName={ planet.name }
+          planetImage={ planet.image }
+        />))}
     </div>
   );
 }
